4e47422bf388d8298d272a0e8a93832d284e5f83
rapyon9 <64957267+rapyon9@users.noreply.github.com>
2021-01-06 21:09:59 +0900

    #Flask 1.1 API테스트

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from kakao_regular import get_kakao_regular
 
 app = Flask(__name__)
-#app.config['JSON_AS_ASCII'] = False #한글이 깨지지 않도록
+app.config['JSON_AS_ASCII'] = False #한글이 깨지지 않도록
  
 @app.route('/userLogin', methods = ['POST'])
 def userLogin():
@@ -10,14 +10,9 @@ def userLogin():
     return jsonify(user)# 받아온 데이터를 다시 전송
  
 @app.route('/')
-def hello_world():
-    return jsonify("hello world")
-
-
-@app.route('/kakao_regular')
-def test() :
-    data = get_kakao_regular()
-    return jsonify(data)
+def kakao_regular() :
+    kakao_regular = get_kakao_regular()
+    return jsonify(kakao_regular)
     
 
 if __name__ == "__main__":
