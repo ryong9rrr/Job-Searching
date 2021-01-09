9d9d33e7a272f9a563fd7eac85c21c8eed129568
rapyon9 <64957267+rapyon9@users.noreply.github.com>
2021-01-09 20:26:05 +0900

    #1.2 KAKAO_상시 모든 페이지 불러오기

--- kakao_ordinary.py
+++ kakao_ordinary.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://careers.kakao.com/jobs?employeeType=Full%20Time&keyword=&page=1"
+URL = "https://careers.kakao.com/jobs?employeeType=Full%20Time&keyword=&"
 
 
 #마지막 페이지를 알아내는 함수
@@ -18,13 +18,17 @@ def get_last_page() :
     return max_page
 
 #현재 페이지의 채용정보의 링크와 제목을 읽어내는 함수
-def get_job() :
-    result = requests.get(URL)
+def extract_jobs(last_page) :
+  jobs = []
+  for page in range(last_page) :
+    print(f"KAKAO page {page+1}")
+    result = requests.get(f"{URL}page={page+1}")
     soup = BeautifulSoup(result.text, 'html.parser')
-    jobs = soup.find("ul", {"class" : "list_jobs"}).find_all("li")
+    print(soup.title)
 
-    for job in jobs :
-        link_title = job.find("a", {"class" : "link_jobs"})
-        print( link_title )
+
+def get_job():
+    last_page = get_last_page()
+    extract_jobs(last_page)
 
 get_job()
