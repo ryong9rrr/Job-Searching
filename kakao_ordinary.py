de1c453831a1a1da5fdeb8a17d469f71edae20dd
rapyon9 <64957267+rapyon9@users.noreply.github.com>
2021-01-09 21:25:19 +0900

    #1.3 KAKAO_상시 모든 공고 title 불러오기

--- kakao_ordinary.py
+++ kakao_ordinary.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = "https://careers.kakao.com/jobs?employeeType=Full%20Time&keyword=&"
+extracted_links = []
 
 
 #마지막 페이지를 알아내는 함수
@@ -17,18 +18,28 @@ def get_last_page() :
     max_page = pages[-1]
     return max_page
 
-#현재 페이지의 채용정보의 링크와 제목을 읽어내는 함수
+#모든 페이지의 채용정보의 링크를 읽어내는 함수
 def extract_jobs(last_page) :
-  jobs = []
-  for page in range(last_page) :
-    print(f"KAKAO page {page+1}")
-    result = requests.get(f"{URL}page={page+1}")
-    soup = BeautifulSoup(result.text, 'html.parser')
-    print(soup.title)
+    for page in range(last_page) :
+        #print(f"KAKAO page {page+1}")
+        result = requests.get(f"{URL}page={page+1}")
+        soup = BeautifulSoup(result.text, 'html.parser')
+        results = soup.find_all("a", {"class" : "link_jobs"})
 
+        for result in results :
+            link = result.get('href')
+            extracted_links.append(link)
+    
+
+def detail_job() :
+    for link in extracted_links :
+        result = requests.get( f"https://careers.kakao.com{link}" )
+        soup = BeautifulSoup(result.text, 'html.parser')
+        print(soup.title)
 
 def get_job():
     last_page = get_last_page()
     extract_jobs(last_page)
-
+    detail_job()
+    
 get_job()
